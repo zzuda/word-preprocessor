867a1b84ca9ce83ac0c88431eba2cb1dc110bb7c
DaegyeomHa <combbm@naver.com>
2021-04-17 14:16:54 +0900

    fix: 파일 이름을 인수로 받을 수 있도록 변경

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import fs from "fs";
 import { read, utils } from "xlsx";
 
+const WORD_COLUNM = "표제어";
+const WORD_PARTS = "품사";
+
 function checkKorean(str: string) {
   for (let i = 0; i < str.length; i++) {
     if (str[i].charCodeAt(0) > 55215) {
@@ -11,8 +14,13 @@ function checkKorean(str: string) {
   return true;
 }
 
-function main(name: string) {
-  const file = fs.readFileSync(`./sheets/${name}.xls`);
+function getFileNameByArgs() {
+  const myArgs = process.argv.slice(2);
+  return myArgs[0];
+}
+
+function main() {
+  const file = fs.readFileSync(getFileNameByArgs());
   const wb = read(file, { type: "buffer" });
   const firstSheet = wb.SheetNames[0];
   const sheet = wb.Sheets[firstSheet];
@@ -24,25 +32,25 @@ function main(name: string) {
   const filteredWord = sheetJson
     .map((i) => {
       const item = i as Record<string, string>;
-      if (item["어휘"].length === 1) return undefined;
-      if (item["어휘"].length >= 4) return undefined;
-      if (item["품사"] !== "명사") return undefined;
-      if (item["어휘"].includes("-")) return undefined;
-      if (!checkKorean(item["어휘"])) return undefined;
+      if (item[WORD_COLUNM].length === 1) return undefined;
+      if (item[WORD_COLUNM].length >= 4) return undefined;
+      if (item[WORD_PARTS] !== "명사") return undefined;
+      if (item[WORD_COLUNM].includes("-")) return undefined;
+      if (!checkKorean(item[WORD_COLUNM])) return undefined;
       return item;
     })
     .filter((i) => i !== undefined)
-    .map((i) => i && i["어휘"]);
+    .map((i) => i && i[WORD_COLUNM]);
   const uniqueWord = new Set(filteredWord);
   const words = [...uniqueWord];
 
   if (!fs.existsSync("./dist")) {
     fs.mkdirSync("./dist");
   }
-  const stream = fs.createWriteStream(`./dist/${name}.txt`);
+  const stream = fs.createWriteStream(`./dist/${Date.now()}.txt`);
   const content = JSON.stringify(words).replace("]", "").replace("[", "");
   stream.write(content);
   stream.end();
 }
 
-main("830239_1050000");
+main();
