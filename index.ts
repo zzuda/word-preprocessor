66e81349df95fe3396ac59fb674fe8671faa6816
DaegyeomHa <combbm@naver.com>
2021-04-17 15:06:55 +0900

    refactor: apply ReadStream

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { read, utils } from "xlsx";
 const WORD_COLUNM = "표제어";
 const WORD_PARTS = "품사";
 
-function checkKorean(str: string) {
+function checkKorean(str: string): boolean {
   for (let i = 0; i < str.length; i++) {
     if (str[i].charCodeAt(0) > 55215) {
       return false;
@@ -14,13 +14,28 @@ function checkKorean(str: string) {
   return true;
 }
 
-function getFileNameByArgs() {
+function getFileNameByArgs(): string {
   const myArgs = process.argv.slice(2);
   return myArgs[0];
 }
 
-function main() {
-  const file = fs.readFileSync(getFileNameByArgs());
+function readSheet(name: string): Promise<Buffer> {
+  const buffers: Buffer[] = [];
+  const readStream = fs.createReadStream(name);
+  return new Promise((resolve, reject) => {
+    readStream.on("data", (c: Buffer) => {
+      buffers.push(c);
+    });
+
+    readStream.on("end", () => {
+      const buffer = Buffer.concat(buffers);
+      resolve(buffer);
+    });
+  });
+}
+
+async function main() {
+  const file = await readSheet(getFileNameByArgs());
   const wb = read(file, { type: "buffer" });
   const firstSheet = wb.SheetNames[0];
   const sheet = wb.Sheets[firstSheet];
@@ -47,10 +62,10 @@ function main() {
   if (!fs.existsSync("./dist")) {
     fs.mkdirSync("./dist");
   }
-  const stream = fs.createWriteStream(`./dist/${Date.now()}.txt`);
+  const writeStream = fs.createWriteStream(`./dist/${Date.now()}.txt`);
   const content = JSON.stringify(words).replace("]", "").replace("[", "");
-  stream.write(content);
-  stream.end();
+  writeStream.write(content);
+  writeStream.end();
 }
 
 main();
